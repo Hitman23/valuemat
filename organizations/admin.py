b9cefe23e682951ffd57cd78a0edfb425968e76f
hitman23 <mugtrade@gmail.com>
2020-04-22 07:57:32 +0300

    Organizations admin interface

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from import_export import fields, resources
+from import_export.admin import ImportExportModelAdmin, ExportActionModelAdmin
+from import_export.widgets import ForeignKeyWidget
 
-# Register your models here.
+from . import models
+
+
+admin.site.site_header = "ValueMat Administration"
+admin.site.site_title = "ValueMat Administration"
+admin.site.index_title = ""
+
+#TODO make it possible for organization data to be imported.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Product)
+class ProductServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.OrganizationBranch)
+class OrganizationBranchAdmin(admin.ModelAdmin):
+    pass
