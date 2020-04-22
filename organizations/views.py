5009e4f62d09d1c473f33c5c5750c822ac130520
hitman23 <mugtrade@gmail.com>
2020-04-22 08:06:06 +0300

    Organizations views setup

--- organizations/views.py
+++ organizations/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, FormView, DeleteView
+from . import models
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = "orgs/index.html"
+
+
+class OrganizationCreateView(CreateView):
+    pass
+
+
+class OrganizationUpdateView(UpdateView):
+    pass
+
+
+class OrganizationDeleteView(DeleteView):
+    pass
